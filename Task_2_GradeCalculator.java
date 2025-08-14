712a93059629b63afe41a12c3eb59dd46825631c
pradeep-xdd <pubgpradeep88@gmail.com>
2025-08-14 22:15:07 +0530

    Update Task_2_GradeCalculator.java

--- Task_2_GradeCalculator.java
+++ Task_2_GradeCalculator.java
@@ -1,52 +1,41 @@
 import java.util.Scanner;
 
 public class Task_2_GradeCalculator {
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
-        System.out.print("Enter number of subjects");
+
+        System.out.print("Enter the number of subjects: ");
         int subjects = sc.nextInt();
-        
-        double totalMarks = 0;
-        
-        System.out.println("Enter marks for each subject (out of 100)");
+
+        int totalMarks = 0;
         for (int i = 1; i <= subjects; i++) {
-            System.out.print("Subject " + i + "=");
-            double marks = sc.nextDouble();
-            
-            if (marks < 0 || marks > 100) {
-                System.out.println("Invalid marks! Please enter between 0-100");
-                i--;
-                continue;
-            }
-            
+            System.out.print("Enter marks obtained in subject " + i + ": ");
+            int marks = sc.nextInt();
             totalMarks += marks;
         }
-        
-        double average = totalMarks / subjects;
-        String grade;
-        
-       
+
+        double average = (double) totalMarks / subjects;
+
+        char grade;
         if (average >= 90) {
-            grade = "A+";
-        } else if (average >= 80) {
-            grade = "A";
+            grade = 'A';
+        } else if (averag >= 80) {
+            grade = 'B';
         } else if (average >= 70) {
-            grade = "B";
+            grade = 'C';
         } else if (average >= 60) {
-            grade = "C";
+            grade = 'D';
         } else if (average >= 50) {
-            grade = "D";
+            grade = 'E';
         } else {
-            grade = "F";
+            grade = 'F';
         }
-        
-        
-        
-        System.out.println("Total Marks=" + totalMarks );
-        System.out.println("Average Percentage="+ average);
-        System.out.println("Grade= " + grade);
-        
+
+        System.out.println("\nTotal Marks: " + totalMarks);
+        System.out.println("Average Percentage: " + average + "%");
+        System.out.println("Grade: " + grade);
+
         
     }
 }
